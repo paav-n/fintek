d10b0b48fcb569439184f3d443e5830a0fffc69f
paav-n <paavannanda@gmail.com>
2018-12-17 14:57:29 -0500

    main project file final commit lolol

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
+from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from pandas_datareader import data as pdr
@@ -8,6 +8,7 @@ import numpy as np
 import fix_yahoo_finance as yf
 import datetime, math, time
 
+plt.style.use('fivethirtyeight')
 today = str(datetime.date.today())
 yf.pdr_override()
 
@@ -15,6 +16,7 @@ yf.pdr_override()
 
 ticker = input("Please enter a ticker:")
 df = pdr.get_data_yahoo(ticker, '2008-01-03', today)#veryyyyyyyy unstable
+ma = df
 df = df[['Open','High','Low','Close','Volume']]
 weekdays = pd.date_range('2008-01-03', today, freq='B')
 df = df.reindex(weekdays)
@@ -22,21 +24,21 @@ df = df.fillna(method='ffill')
 
 ####################calculate moving averages and other data####################
 
-#df['50day'] = df.Close.rolling(50).mean().shift()
-#df['200day'] = df.Close.rolling(200).mean().shift()
+ma['50day'] = df.Close.rolling(50).mean().shift()
+ma['200day'] = df.Close.rolling(200).mean().shift()
 df['HL%'] = (df['High'] - df['Low']) / df['Low'] * 100
 df['%Change'] = (df['Close'] - df['Open']) / df['Open'] * 100
 
 df = df[['Close','HL%','%Change','Volume']]
 df.fillna(-99999, inplace=True)
-#backup = df
+backup = df
 #print(df.iloc[20:60,0:3])
 
 #####################analyze and train####################
 
 length = len(df)
 forecast_col = 'Close'
-forecast_out = int(math.ceil(0.01*length))
+forecast_out = int(math.ceil(0.1*length))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 #print(df.tail())
@@ -64,12 +66,12 @@ df['forecast'] = np.nan
 today = df.iloc[-1].name
 last = today.timestamp()
 day = 86400
-next = last + day
+nextraw = last + day
 
 length1 = len(df.columns)
 for i in prediction:
-    nextday = datetime.datetime.fromtimestamp(next)
-    next += day
+    nextday = datetime.datetime.fromtimestamp(nextraw)
+    nextraw += day
     df.loc[nextday] = [np.nan for all in range(length1 - 1)] + [i]
 
 #####################plot data####################
@@ -78,8 +80,8 @@ plt.figure(figsize=(15,10))
 plt.grid(True)
 plt.plot(df['Close'], label=ticker)
 plt.plot(df['forecast'], label='future')
-#plt.plot(df['50day'], label='50 day')
-#plt.plot(df['200day'], label='200 day')
+plt.plot(ma['50day'], label='50 day')
+plt.plot(ma['200day'], label='200 day')
 plt.plot(df['HL%'], label='HL %')
 plt.plot(df['%Change'], label='% Change')
 plt.legend(loc=2)
